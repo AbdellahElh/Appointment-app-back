4694b327d629ea12fbe1c12adbaf26983279c6dd
Abdellah El Halimi <abdellah.elhalimimerroun@student.hogent.be>
2023-12-19 23:38:22 +0100

    Refactor code for improved performance

--- __tests__/rest/doctor.spec.js
+++ __tests__/rest/doctor.spec.js
@@ -29,58 +29,28 @@ const data = {
       id: 1,
       name: "Dr. Olivia Anderson",
       speciality: "Cardiologist",
-      // numberOfPatients: 3, // this week
       photo: "../assets/imgs/doc1.jpg",
       hospital: "AZ Groeninge",
-      // numberOfRatings: 5,
-      rating: "4.8",
       about:
         "Dr. Olivia Anderson is a dedicated and experienced cardiologist...",
-      // timeSlots: [
-      //   { day: "Monday", time: "8:00 - 16:00" },
-      //   { day: "Tuesday", time: "14:30 - 18:30" },
-      //   { day: "Wednesday", time: "10:00 - 18:00" },
-      //   { day: "Thursday", time: "9:30 - 13:00" },
-      //   { day: "Friday", time: "13:30 - 18:00" },
-      // ],
     },
     {
       id: 2,
       name: "Dr. Michael Brown Smith",
       speciality: "Dentist",
-      // numberOfPatients: 2,
       photo: "../assets/imgs/doc2.jpg",
       hospital: "AZ Sint-Jan Brugge-Oostende",
-      // numberOfRatings: 6,
-      rating: "4.5",
       about:
         "Dr. Michael Brown Smith is a highly skilled dentist at AZ Sint-Jan Brugge-Oostende...",
-      // timeSlots: [
-      //   { day: "Saturday", time: "9:00 - 17:00" },
-      //   { day: "Sunday", time: "14:00 - 17:30" },
-      //   { day: "Monday", time: "14:00 - 18:30" },
-      //   { day: "Tuesday", time: "9:30 - 12:00" },
-      //   { day: "Wednesday", time: "13:30 - 17:30" },
-      // ],
     },
     {
       id: 3,
       name: "Dr. John Davis Wilson",
       speciality: "Orthopedic Surgeon",
-      // numberOfPatients: 1,
       photo: "../assets/imgs/doc3.jpg",
       hospital: "AZ Turnhout",
-      // numberOfRatings: 7,
-      rating: "4.7",
       about:
         "Dr. John Davis Wilson is a renowned orthopedic surgeon at AZ Turnhout...",
-      // timeSlots: [
-      //   { day: "Monday", time: "15:30 - 19:30" },
-      //   { day: "Tuesday", time: "8:30 - 16:30" },
-      //   { day: "Wednesday", time: "14:00 - 17:30" },
-      //   { day: "Thursday", time: "11:30 - 15:00" },
-      //   { day: "Friday", time: "16:00 - 20:00" },
-      // ],
     },
   ],
 };
@@ -107,11 +77,9 @@ describe("Doctors", () => {
 
   describe("GET /api/doctors", () => {
     beforeAll(async () => {
-      // Check if the user already exists
       for (const user of data.users) {
         const userExists = await knex(tables.user).where("id", user.id).first();
 
-        // Only insert the user if it doesn't already exist
         if (!userExists) {
           await knex(tables.user).insert(user);
         }
@@ -143,40 +111,20 @@ describe("Doctors", () => {
         email: "olivia.anderson@gmail.com",
         roles: [Role.DOCTOR],
         speciality: "Cardiologist",
-        // numberOfPatients: 3,
         photo: "../assets/imgs/doc1.jpg",
         hospital: "AZ Groeninge",
-        // numberOfRatings: 5,
-        rating: "4.8",
         about:
           "Dr. Olivia Anderson is a dedicated and experienced cardiologist...",
-        // timeSlots: [
-        //   { day: "Monday", time: "8:00 - 16:00" },
-        //   { day: "Tuesday", time: "14:30 - 18:30" },
-        //   { day: "Wednesday", time: "10:00 - 18:00" },
-        //   { day: "Thursday", time: "9:30 - 13:00" },
-        //   { day: "Friday", time: "13:30 - 18:00" },
-        // ],
       });
       expect(response.body.items[0]).toEqual({
         id: 3,
         name: "Dr. John Davis Wilson",
         email: "john.wilson@gmailcom",
         speciality: "Orthopedic Surgeon",
-        // numberOfPatients: 1,
         photo: "../assets/imgs/doc3.jpg",
         hospital: "AZ Turnhout",
-        // numberOfRatings: 7,
-        rating: "4.7",
         about:
           "Dr. John Davis Wilson is a renowned orthopedic surgeon at AZ Turnhout...",
-        // timeSlots: [
-        //   { day: "Monday", time: "15:30 - 19:30" },
-        //   { day: "Tuesday", time: "8:30 - 16:30" },
-        //   { day: "Wednesday", time: "14:00 - 17:30" },
-        //   { day: "Thursday", time: "11:30 - 15:00" },
-        //   { day: "Friday", time: "16:00 - 20:00" },
-        // ],
       });
     });
     it("should 400 when given an argument", async () => {
@@ -194,7 +142,6 @@ describe("Doctors", () => {
   describe("GET /api/doctor/:id", () => {
     beforeAll(async () => {
       await knex(tables.user).insert(data.users);
-      // Convert timeSlots to a JSON string before inserting
       await knex(tables.doctor).insert(doctor);
     });
 
@@ -212,20 +159,10 @@ describe("Doctors", () => {
         id: 1,
         name: "Dr. Olivia Anderson",
         speciality: "Cardiologist",
-        // numberOfPatients: 3,
         photo: "../assets/imgs/doc1.jpg",
         hospital: "AZ Groeninge",
-        // numberOfRatings: 5,
-        rating: "4.8",
         about:
           "Dr. Olivia Anderson is a dedicated and experienced cardiologist...",
-        // timeSlots: [
-        //   { day: "Monday", time: "8:00 - 16:00" },
-        //   { day: "Tuesday", time: "14:30 - 18:30" },
-        //   { day: "Wednesday", time: "10:00 - 18:00" },
-        //   { day: "Thursday", time: "9:30 - 13:00" },
-        //   { day: "Friday", time: "13:30 - 18:00" },
-        // ],
       });
     });
 
@@ -261,7 +198,6 @@ describe("Doctors", () => {
     const doctorsToDelete = [];
 
     afterAll(async () => {
-      // Delete the new doctors
       await knex(tables.doctor).whereIn("id", doctorsToDelete).delete();
     });
 
@@ -278,13 +214,8 @@ describe("Doctors", () => {
       expect(response.body.user.email).toBe("doctor@user.com");
       expect(response.body.user.roles).toContain("DOCTOR");
       expect(response.body.user.speciality).toBe("Default Speciality");
-      // expect(response.body.user.numberOfPatients).toBe(0);
-      expect(response.body.user.photo).toBe("https://i.imgur.com/2WZtVXx.png");
       expect(response.body.user.hospital).toBe("Default Hospital");
-      // expect(response.body.user.numberOfRatings).toBe(0);
-      expect(response.body.user.rating).toBe("0");
       expect(response.body.user.about).toBeNull();
-      // expect(response.body.user.timeSlots).toBeNull();
       expect(response.body.token).toBeTruthy();
 
       doctorsToDelete.push(response.body.user.id);
@@ -305,7 +236,6 @@ describe("Doctors", () => {
   describe("PUT /api/doctors/:id", () => {
     beforeAll(async () => {
       await knex(tables.user).insert(data.users);
-      // Convert timeSlots to a JSON string before inserting
       await knex(tables.doctor).insert(doctor);
     });
 
@@ -321,17 +251,9 @@ describe("Doctors", () => {
         .send({
           name: "Changed name",
           speciality: "updated speciality",
-          // numberOfPatients: 3,
           photo: "../assets/imgs/doc1.jpg",
           hospital: "Updated Hospital",
-          // numberOfRatings: 6,
-          rating: "4.9",
           about: "Updated about...",
-          // timeSlots: [
-          //   { day: "Monday", time: "10:00 - 18:00" },
-          //   { day: "Tuesday", time: "14:00 - 20:00" },
-          //   { day: "Wednesday", time: "8:30 - 16:30" },
-          // ],
         });
 
       expect(response.statusCode).toBe(200);
@@ -339,17 +261,9 @@ describe("Doctors", () => {
         id: 1,
         name: "Changed name",
         speciality: "updated speciality",
-        // numberOfPatients: 3,
         photo: "../assets/imgs/doc1.jpg",
         hospital: "Updated Hospital",
-        // numberOfRatings: 6,
-        rating: "4.9",
         about: "Updated about...",
-        // timeSlots: [
-        //   { day: "Monday", time: "10:00 - 18:00" },
-        //   { day: "Tuesday", time: "14:00 - 20:00" },
-        //   { day: "Wednesday", time: "8:30 - 16:30" },
-        // ],
       });
     });
 
@@ -360,17 +274,9 @@ describe("Doctors", () => {
         .set("Authorization", adminAuthHeader)
         .send({
           speciality: "updated speciality",
-          // numberOfPatients: 3,
           photo: "../assets/imgs/doc1.jpg",
           hospital: "Updated Hospital",
-          // numberOfRatings: 6,
-          rating: "4.9",
           about: "Updated about...",
-          // timeSlots: [
-          //   { day: "Monday", time: "10:00 - 18:00" },
-          //   { day: "Tuesday", time: "14:00 - 20:00" },
-          //   { day: "Wednesday", time: "8:30 - 16:30" },
-          // ],
         });
 
       expect(response.statusCode).toBe(400);
@@ -385,17 +291,9 @@ describe("Doctors", () => {
         .set("Authorization", adminAuthHeader)
         .send({
           name: "Changed name",
-          // numberOfPatients: 3,
           photo: "../assets/imgs/doc1.jpg",
           hospital: "Updated Hospital",
-          // numberOfRatings: 6,
-          rating: "4.9",
           about: "Updated about...",
-          // timeSlots: [
-          //   { day: "Monday", time: "10:00 - 18:00" },
-          //   { day: "Tuesday", time: "14:00 - 20:00" },
-          //   { day: "Wednesday", time: "8:30 - 16:30" },
-          // ],
         });
 
       expect(response.statusCode).toBe(400);
@@ -410,16 +308,8 @@ describe("Doctors", () => {
         .send({
           name: "Changed name",
           speciality: "updated speciality",
-          // numberOfPatients: 3,
           hospital: "Updated Hospital",
-          // numberOfRatings: 6,
-          rating: "4.9",
           about: "Updated about...",
-          // timeSlots: [
-          //   { day: "Monday", time: "10:00 - 18:00" },
-          //   { day: "Tuesday", time: "14:00 - 20:00" },
-          //   { day: "Wednesday", time: "8:30 - 16:30" },
-          // ],
         });
 
       expect(response.statusCode).toBe(400);
@@ -434,45 +324,13 @@ describe("Doctors", () => {
         .send({
           name: "Changed name",
           speciality: "updated speciality",
-          // numberOfPatients: 3,
           photo: "../assets/imgs/doc1.jpg",
-          // numberOfRatings: 6,
-          rating: "4.9",
           about: "Updated about...",
-          // timeSlots: [
-          //   { day: "Monday", time: "10:00 - 18:00" },
-          //   { day: "Tuesday", time: "14:00 - 20:00" },
-          //   { day: "Wednesday", time: "8:30 - 16:30" },
-          // ],
         });
 
       expect(response.statusCode).toBe(400);
       expect(response.body.code).toBe("VALIDATION_FAILED");
       expect(response.body.details.body).toHaveProperty("hospital");
-    });
-
-    it("should return 400 when missing rating", async () => {
-      const response = await request
-        .put(`${url}/1`)
-        .set("Authorization", adminAuthHeader)
-        .send({
-          name: "Changed name",
-          speciality: "updated speciality",
-          // numberOfPatients: 3,
-          photo: "../assets/imgs/doc1.jpg",
-          hospital: "Updated Hospital",
-          // numberOfRatings: 6,
-          about: "Updated about...",
-          // timeSlots: [
-          //   { day: "Monday", time: "10:00 - 18:00" },
-          //   { day: "Tuesday", time: "14:00 - 20:00" },
-          //   { day: "Wednesday", time: "8:30 - 16:30" },
-          // ],
-        });
-
-      expect(response.statusCode).toBe(400);
-      expect(response.body.code).toBe("VALIDATION_FAILED");
-      expect(response.body.details.body).toHaveProperty("rating");
     });
 
     it("should return 400 when missing about", async () => {
@@ -482,16 +340,8 @@ describe("Doctors", () => {
         .send({
           name: "Changed name",
           speciality: "updated speciality",
-          // numberOfPatients: 3,
           photo: "../assets/imgs/doc1.jpg",
           hospital: "Updated Hospital",
-          // numberOfRatings: 6,
-          rating: "4.9",
-          // timeSlots: [
-          //   { day: "Monday", time: "10:00 - 18:00" },
-          //   { day: "Tuesday", time: "14:00 - 20:00" },
-          //   { day: "Wednesday", time: "8:30 - 16:30" },
-          // ],
         });
 
       expect(response.statusCode).toBe(400);
@@ -504,7 +354,6 @@ describe("Doctors", () => {
   describe("DELETE /api/doctors/:id", () => {
     beforeAll(async () => {
       await knex(tables.user).insert(data.users);
-      // Convert timeSlots to a JSON string before inserting
       await knex(tables.doctor).insert(doctor);
     });
 
