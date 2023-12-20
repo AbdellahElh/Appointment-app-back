62a3ce8494ab417d26adfff52ebfb3e5d37a53c0
Abdellah El Halimi <abdellah.elhalimimerroun@student.hogent.be>
2023-12-20 01:44:50 +0100

    Add doctor images to seed data

--- src/data/seeds/202311061200_doctors.js
+++ src/data/seeds/202311061200_doctors.js
@@ -1,4 +1,7 @@
 const { tables } = require("..");
+const doc1 = require("../../assets/imgs/doc1.jpg");
+const doc2 = require("../../assets/imgs/doc2.jpg");
+const doc3 = require("../../assets/imgs/doc3.jpg");
 
 module.exports = {
   seed: async (knex) => {
@@ -8,7 +11,7 @@ module.exports = {
         id: 10,
         name: "Dr. Olivia Anderson",
         speciality: "Cardiologist",
-        photo: "../assets/imgs/doc1.jpg",
+        photo: doc1,
         hospital: "AZ Groeninge",
         about:
           "Dr. Olivia Anderson is a dedicated and experienced cardiologist, currently practicing at AZ Groeninge. With a patient-first approach, she has successfully treated numerous patients, earning a 5-star rating. Her commitment to her profession is reflected in the positive health outcomes of her patients. She continually strives to provide the best cardiac care, keeping herself updated with the latest in cardiology.",
@@ -17,7 +20,7 @@ module.exports = {
         id: 11,
         name: "Dr. Michael Brown Smith",
         speciality: "Dentist",
-        photo: "../assets/imgs/doc2.jpg",
+        photo: doc2,
         hospital: "AZ Sint-Jan Brugge-Oostende",
         about:
           "Dr. Michael Brown Smith is a highly skilled dentist at AZ Sint-Jan Brugge-Oostende. He has a patient-centric approach and is known for his gentle and efficient dental procedures. His patients appreciate his thoroughness and his commitment to their dental health, earning him a 4.5-star rating.",
@@ -26,7 +29,7 @@ module.exports = {
         id: 12,
         name: "Dr. John Davis Wilson",
         speciality: "Orthopedic Surgeon",
-        photo: "../assets/imgs/doc3.jpg",
+        photo: doc3,
         hospital: "AZ Turnhout",
         about:
           "Dr. John Davis Wilson is a renowned orthopedic surgeon at AZ Turnhout. He specializes in diagnosing and treating disorders related to the musculoskeletal system. His dedication to his patients and his field is evident in his work, earning him a 4-star rating. He is committed to helping his patients regain mobility and improve their quality of life.",
