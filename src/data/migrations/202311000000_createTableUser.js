e53e64451bde58458ce7c29cd0ba7a9dd3bf11d4
Abdellah El Halimi <abdellah.elhalimimerroun@student.hogent.be>
2024-12-08 20:01:00 +0100

    removed some comments

--- src/data/migrations/202311000000_createTableUser.js
+++ src/data/migrations/202311000000_createTableUser.js
@@ -1,23 +1,16 @@
-// Ik gebruikt Knex.js om databasetabellen te creëren en te wijzigen.
-// Bijvoorbeeld, in migratiebestand 202311000000_createTableUser.js, gebruikt ik knex.schema.createTable
-// om de user tabel te creëren:
 const { tables } = require("../index.js");
 
 module.exports = {
-  // De 'up' functie wordt uitgevoerd wanneer de migratie wordt uitgevoerd
   up: async (knex) => {
-    // We gebruiken knex.schema.createTable om een nieuwe tabel te maken
     await knex.schema.createTable(tables.user, (table) => {
-      table.increments("id"); // Een auto-increment ID veld
-      table.string("email").notNullable(); // Een verplicht 'email' veld
-      table.string("password_hash").notNullable(); // Een verplicht 'password_hash' veld
-      table.jsonb("roles").notNullable(); // Een verplicht 'roles' veld dat een JSONB datatype heeft
-      table.unique("email", "idx_user_email_unique"); // Een unieke index op het 'email' veld
+      table.increments("id");
+      table.string("email").notNullable();
+      table.string("password_hash").notNullable();
+      table.jsonb("roles").notNullable();
+      table.unique("email", "idx_user_email_unique");
     });
   },
-  // De 'down' functie wordt uitgevoerd wanneer de migratie wordt teruggedraaid
   down: (knex) => {
-    // We gebruiken knex.schema.dropTableIfExists om de tabel te verwijderen als deze bestaat
     return knex.schema.dropTableIfExists(tables.user);
   },
 };
--- src/data/seeds/202311000000_users.js
+++ src/data/seeds/202311000000_users.js
@@ -2,16 +2,11 @@ const { tables } = require("..");
 const Role = require("../../core/roles");
 
 module.exports = {
-  // De 'seed' functie wordt uitgevoerd om de initiële data in de tabel te plaatsen
   seed: async (knex) => {
-    // We gebruiken knex(tables.user).delete() om alle bestaande data uit de tabel te verwijderen
     await knex(tables.user).delete();
-    // We gebruiken knex(tables.user).insert() om nieuwe data in de tabel te plaatsen
     await knex(tables.user).insert([
-      // Elke object in deze array vertegenwoordigt een rij in de tabel
       {
         id: 5,
-        // name: "Abdellah El Halimi Merroun",
         email: "abdellah.elhalimimerroun@student.hogent.be",
         password_hash:
           "$argon2id$v=19$m=131072,t=6,p=1$9AMcua9h7va8aUQSEgH/TA$TUFuJ6VPngyGThMBVo3ONOZ5xYfee9J1eNMcA5bSpq4",
@@ -19,7 +14,6 @@ module.exports = {
       },
       {
         id: 1,
-        // name: "Emily Smith",
         email: "emily.smith@gmail.com",
         password_hash:
           "$argon2id$v=19$m=131072,t=6,p=1$9AMcua9h7va8aUQSEgH/TA$TUFuJ6VPngyGThMBVo3ONOZ5xYfee9J1eNMcA5bSpq4",
@@ -27,7 +21,6 @@ module.exports = {
       },
       {
         id: 2,
-        // name: "David Brown",
         email: "david.brown@gmail.com",
         password_hash:
           "$argon2id$v=19$m=131072,t=6,p=1$9AMcua9h7va8aUQSEgH/TA$TUFuJ6VPngyGThMBVo3ONOZ5xYfee9J1eNMcA5bSpq4",
@@ -35,7 +28,6 @@ module.exports = {
       },
       {
         id: 3,
-        // name: "Sophia Davis",
         email: "sophia.davis@gmail.com",
         password_hash:
           "$argon2id$v=19$m=131072,t=6,p=1$9AMcua9h7va8aUQSEgH/TA$TUFuJ6VPngyGThMBVo3ONOZ5xYfee9J1eNMcA5bSpq4",
@@ -45,7 +37,6 @@ module.exports = {
       //DOCTORS
       {
         id: 10,
-        // name: "Olivia Anderson",
         email: "olivia.anderson@gmail.com",
         password_hash:
           "$argon2id$v=19$m=131072,t=6,p=1$9AMcua9h7va8aUQSEgH/TA$TUFuJ6VPngyGThMBVo3ONOZ5xYfee9J1eNMcA5bSpq4",
@@ -53,7 +44,6 @@ module.exports = {
       },
       {
         id: 11,
-        // name: "Michael Brown",
         email: "michael.brown@gmail.com",
         password_hash:
           "$argon2id$v=19$m=131072,t=6,p=1$9AMcua9h7va8aUQSEgH/TA$TUFuJ6VPngyGThMBVo3ONOZ5xYfee9J1eNMcA5bSpq4",
@@ -61,7 +51,6 @@ module.exports = {
       },
       {
         id: 12,
-        // name: "John Wilson",
         email: "john.wilson@gmail.com",
         password_hash:
           "$argon2id$v=19$m=131072,t=6,p=1$9AMcua9h7va8aUQSEgH/TA$TUFuJ6VPngyGThMBVo3ONOZ5xYfee9J1eNMcA5bSpq4",
